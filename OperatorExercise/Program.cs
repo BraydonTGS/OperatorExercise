e263c2ba62f11c2757f7d6dc9364f244f83d6592
Braydon Sutherland <BraydonSutherland@gmail.com>
2022-09-13 10:38:47 -0500

    combined the two programs into one based on user input, added spacing in the console

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,30 +4,36 @@ do
 {
     double num1 = 0;
     double num2 = 0;
-    double radius = 0;
     double area = 0;
     double answer = 0;
-
+    Console.Clear();
     Console.WriteLine("*******************");
     Console.WriteLine("Calculator Program");
     Console.WriteLine("*******************");
     Console.WriteLine();
     Console.WriteLine("1: Standard Calculator");
-    Console.WriteLine("2: Area of a Cirlce");
+    Console.WriteLine(); 
+    Console.WriteLine("2: Area of a Cirlce ");
+    Console.WriteLine();
     string chooseProgram = Console.ReadLine();
 
     if (chooseProgram == "1")
     {
+        Console.WriteLine();
         Console.Write("Please Enter number 1: ");
         num1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine();
         Console.Write("Please Enter number 2: ");
         num2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine();
         Console.WriteLine("Please Pick From the Following");
+        Console.WriteLine();
         Console.WriteLine("\t+ : Add");
         Console.WriteLine("\t- : Subtract");
         Console.WriteLine("\t* : Multiply");
         Console.WriteLine("\t/ : Divide");
         Console.WriteLine("\t% : Modulus");
+        Console.WriteLine();
         Console.Write("Enter an Operator ");
         string userInput = Console.ReadLine();
 
@@ -49,32 +55,47 @@ do
                 answer = num1 % num2;
                 break;
             default:
+                Console.WriteLine();
                 Console.WriteLine("Please enter the right operator!!! ");
                 break;
 
         }
+        Console.WriteLine();
         Console.WriteLine($"The result is {answer}");
+        Console.WriteLine();
         Console.Write("Would you like to continue(Y/N)? ");
     }
-    else
+    else if (chooseProgram == "2")
     {
+        Console.WriteLine();
         Console.Write("Please Enter the Radius of a Circle: ");
         area = Convert.ToDouble(Console.ReadLine());
         area = Math.Round(AreaOfACirlce(area), 2);
-        Console.WriteLine($"The Area of the Cirlce is {area}"); 
+        Console.WriteLine();
+        Console.WriteLine($"The Area of the Cirlce is {area}");
+        Console.WriteLine();
 
-         static double AreaOfACirlce(double userInput)
+        static double AreaOfACirlce(double userInput)
         {
-            userInput = Math.PI * Math.Pow(userInput, 2); 
-            return userInput; 
+            userInput = Math.PI * Math.Pow(userInput, 2);
+            return userInput;
         }
-
+        Console.Write("Would you like to continue(Y/N)? ");
     }
-    Console.Write("Would you like to continue(Y/N)? ");
 
-}       while (Console.ReadLine().ToUpper() == "Y");
-        Console.WriteLine("Thank You!!!");
-        Console.ReadLine();
+    else
+    {
+        Console.WriteLine();
+        Console.WriteLine("Please Select 1 or 2!!! ");
+        Console.WriteLine();
+        Console.Write("Would you like to continue(Y/N)? ");
+    }
+
+}
+
+while (Console.ReadLine().ToUpper() == "Y");
+Console.WriteLine("Thank You!!!");
+Console.ReadLine();
 
 
 
